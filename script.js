c6ba95b35b786264fbcb1494c60fa9bd73a88e93
keliancombes <keliancombes@gmail.com>
2025-11-04 15:01:16 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Laboratoire prêt !");
+const nom = "combes";
+let age = 16;
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123";
+if ( motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length < 8 ) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
